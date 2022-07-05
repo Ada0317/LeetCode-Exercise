c76c1e7781a743a17d12c2d5bfee85c6ec357068
Ada0317 <03170884@cumt.edu.cn>
2022-07-05 17:14:18 +0800

    20220704:new:剑指offer

--- hard/4  寻找两个正序数组的中位数.go
+++ hard/4  寻找两个正序数组的中位数.go
@@ -55,3 +55,42 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	return float64(t1+t2) / 2
 
 }
+
+// 二分查找法
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
+	m := len(nums1)
+	n := len(nums2)
+
+	left := (n + m + 1) / 2
+	right := (n + m + 2) / 2
+	return float64(getK(nums1, 0, m-1, nums2, 0, n-1, left)+getK(nums1, 0, m-1, nums2, 0, n-1, right)) * 0.5
+}
+
+func getK(nums1 []int, start1 int, end1 int, nums2 []int, start2 int, end2 int, k int) int {
+	len1 := end1 - start1 + 1
+	len2 := end2 - start2 + 1
+	if len1 > len2 {
+		return getK(nums2, start2, end2, nums1, start1, end1, k)
+	}
+	if len1 == 0 {
+		return nums2[start2+k-1]
+	}
+	if k == 1 {
+		return getMin(nums2[start2], nums1[start1])
+	}
+	i := start1 + getMin(len1, k/2) - 1
+	j := start2 + getMin(len2, k/2) - 1
+	if nums1[i] > nums2[j] {
+		return getK(nums1, start1, end1, nums2, j+1, end2, k-(j-start2+1))
+	} else {
+		return getK(nums1, i+1, end1, nums2, start2, end2, k-(i-start1+1))
+	}
+}
+
+func getMin(n1, n2 int) int {
+	if n1 > n2 {
+		return n2
+	} else {
+		return n1
+	}
+}
